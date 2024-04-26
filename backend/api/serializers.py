d75e7c03ef0c206fcefc2b85ae7370ed3eb77b41
Arya Shakya <ryaashakya@gmail.com>
2024-04-26 22:32:20 +0545

    update serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,4 +5,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ["id","title","created_at"]
+        fields = ["id","title","created_at","completed"]
