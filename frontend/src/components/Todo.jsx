b3fb69e069610f98e660d726e4e819784967e8e8
Arya Shakya <ryaashakya@gmail.com>
2024-04-26 08:13:46 +0545

    Create Todo component

---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Todo({ todo, onDelete }) {
+  const formattedDate = new Date(todo.created_at).toLocaleDateString("en-US");
+
+  return (
+    <div className="todo-container">
+      <p className="todo-title">{todo.title}</p>
+      <p className="todo-date">{formattedDate}</p>
+      <button className="delete-button" onClick={() => onDelete(todo.id)}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default Todo;
