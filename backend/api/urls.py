78077f8ca3299c5e37f5e6fe100b0ecd73022aaf
Arya Shakya <ryaashakya@gmail.com>
2024-04-26 22:31:31 +0545

    Add update url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("todos/", views.TodoListCreate.as_view(), name="todo-list"),
     path("todos/delete/<int:pk>/", views.TodoDelete.as_view(), name="delete-todo"),
+    path("todos/update/<int:pk>/", views.TodoUpdate.as_view(), name="update-todo"),
 ]
