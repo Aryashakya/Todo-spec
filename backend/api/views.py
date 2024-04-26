7f3f902e304577bebc178107cbd4d3d846051733
Arya Shakya <ryaashakya@gmail.com>
2024-04-26 22:31:14 +0545

    Add update view

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,18 +3,25 @@ from .models import Todo
 from .serializers import TodoSerializer
 from rest_framework import generics
 
+
 class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
-    
+
     def get_queryset(self):
         return Todo.objects.all()
 
-    
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         if serializer.is_valid():
             serializer.save()
         else:
             print(serializer.errors)
+
+
+class TodoUpdate(generics.UpdateAPIView):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+    lookup_field = "pk"
+
 
 class TodoDelete(generics.DestroyAPIView):
     serializer_class = TodoSerializer
