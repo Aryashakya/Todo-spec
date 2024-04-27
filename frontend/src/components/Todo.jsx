e9d060b2cfb9aa20f9a9bddc3c59c66b2fa7e5b2
Arya Shakya <ryaashakya@gmail.com>
2024-04-27 10:30:50 +0545

    Add update and toggle functionality

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "../styles/Todo.css";
 
-function Todo({ todo, onDelete }) {
+function Todo({ todo, onToggle, onUpdate, onDelete }) {
   const formattedDate = new Date(todo.created_at).toLocaleDateString("en-US");
 
   return (
     <div className="todo-container">
       <p className="todo-title">{todo.title}</p>
       <p className="todo-date">{formattedDate}</p>
+      {todo.completed ? <p>Completed</p> : <p>Todo</p>}
+      <button
+        className="delete-button"
+        onClick={() => onToggle(todo.id, todo.completed)}
+      >
+        Toggle
+      </button>
+      <button
+        className="delete-button"
+        onClick={() => onUpdate(todo.id, todo.title)}
+      >
+        Edit
+      </button>
       <button className="delete-button" onClick={() => onDelete(todo.id)}>
         Delete
       </button>
